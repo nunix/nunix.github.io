697874d12fc35d8bae92d6a9a49e6f4ecd4cb5c6
nunix <corsair@hey.com>
2025-04-15 22:02:03 +0200

    Added the base option in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://nunix.github.io',
+    base: '/',
     output: 'static',
     trailingSlash: 'always',
     integrations: [starlight({
