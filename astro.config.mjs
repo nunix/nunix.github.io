74245d11997bfeb7fc8cb9725a1fb5a53138e95f
nunix <corsair@hey.com>
2025-04-15 21:52:54 +0200

    Initial Astro blog commit

---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+import mdx from '@astrojs/mdx';
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [starlight({
+        title: 'The Corsair',
+        social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/nunix' }],
+        sidebar: [
+            {
+                label: 'Guides',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Example Guide', slug: 'guides/example' },
+                ],
+            },
+            {
+                label: 'Reference',
+                autogenerate: { directory: 'reference' },
+            },
+        ],
+		}), mdx()],
+});
